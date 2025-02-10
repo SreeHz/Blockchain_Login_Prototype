8040b03c74125b23d62c67116c60ee67d4e1d436
SreeHz <raswanth28@gmail.com>
2025-02-10 19:16:37 +0530

    del sample

--- generateKeys.js
+++
@@ -1,8 +0,0 @@
-const ethers = require("ethers");
-
-// Generate a new wallet (key pair)
-const wallet = ethers.Wallet.createRandom();
-
-console.log("Public Key (Wallet Address):", wallet.address);
-console.log("Private Key:", wallet.privateKey);
-
